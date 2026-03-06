93443991ae5aaf81061f04e3bbb4addb2cb816b7
Emeline <emeline.bouche06@gmail.com>
2026-03-06 11:35:31 +0100

    Ajout des epreuve 1 et 2 + enlever fichier test

---
+++ epreuve2_cachettes.py
@@ -0,0 +1,10 @@
+'''Description de l'épreuve: Jeu de la cachette (se libère une fois la lumière allumée: epreuve 1 validée)
+        -  Bruit
+        - Se cacher dans une des cachettes proposés ( 3 cachette se libère sur 6 possibles )
+        - Un monstre découvre une cachette aléatoirement
+        - Si cachette joueur découverte 
+        - Mort
+        - Retour au début
+        - Sinon deuxième partie du code pour ouvrir la porte 
+
+'''
