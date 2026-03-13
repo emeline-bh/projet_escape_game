5dbcd6f67ad4a9764bc2d39b871c9d072cdc7a3d
Emeline <emeline.bouche06@gmail.com>
2026-03-13 09:37:03 +0100

    Ajout de la grande fenetre et de la bienvenue

--- main.py
+++ main.py
@@ -2,5 +2,14 @@
     - Local escape game (fentre principale + salle 1 + salle 2)
      
 """
+from tkinter import * 
+grande_fenetre= Tk()
+grande_fenetre.title ("Escape game")
 
-print("coucou solene mbb")
+grande_fenetre.geometry("500x500")
+grande_fenetre.minsize(300,200)
+
+grande_fenetre.iconbitmap("logomaison.jpg")
+
+        
+grande_fenetre.mainloop()
