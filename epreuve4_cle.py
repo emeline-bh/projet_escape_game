1bd48cb7847339b0e16f3499cc4c33be1c8fb3df
So-Ber44 <solene.bernard@reseau.eseo.fr>
2026-03-13 08:49:57 +0100

    ajout tirets cosigne epreuve 4

--- epreuve4_cle.py
+++ epreuve4_cle.py
@@ -1,13 +1,14 @@
 '''Description de l'épreuve des clés
 
 
-        affichage d'une clé pendant 2 sec
-        affichage d'une sélections de clés toutes différentes avec la bonne dedans
-        10 sec pour choisir la bonne  +  3 essais max
-            si raté vous vous retrouvez enfermé dans la pièce 
-                retour au début
-            si réussi 
-                ouverture de la porte
+        - affichage d'une clé pendant 2 sec
+        - affichage d'une sélections de clés toutes différentes avec la bonne dedans
+        - 10 sec pour choisir la bonne  +  3 essais max
+            - si raté vous vous retrouvez enfermé dans la pièce 
+                - retour au début
+            - si réussi 
+                - ouverture de la porte
 
                 
 '''
+
